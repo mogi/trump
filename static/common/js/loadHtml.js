954f5ef529ce3c8633e15b09a08aecf5ef0e72f8
Hiroki Mogi <mogi@gu3.co.jp>
2013-01-30 21:15:53 +0900

    配置換え

--- static/common/js/loadHtml.js
+++ static/common/js/loadHtml.js
@@ -1,4 +1,33 @@
 // htmlファイルを遅延読み込み
+
+function displayData() {
+  if ((httpObj.readyState == 4) && (httpObj.status == 200)) {
+    $("result").innerHTML = httpObj.responseText;
+  }else{
+    $("result").innerHTML = "<b>Loading...</b>";
+  }
+}
+
+
+function createXMLHttpRequest() {
+  if (window.XMLHttpRequest) {
+    return new XMLHttpRequest();
+  } else if (window.ActiveXObject) {
+    try {
+      return new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+      try {
+        return new ActiveXObject("Microsoft.XMLHTTP");
+      } catch (e2) {
+        return null;
+      }
+    }
+  } else {
+    return null;
+  }
+}
+
+
 function loadHTMLFile(fName) {
   httpObj = createXMLHttpRequest(displayData);
   if (httpObj) {
--- static/trump/import.js
+++ static/trump/import.js
@@ -7,20 +7,10 @@
     var importSrc = function(e){
         return '<script type="text/javascript" src="'+ e +'"></script>' + "\n";
     }
-    var importModule = function(m){
-      var path = './static/';
-      document.write( importSrc( path + m + '/import.js' ) );
-    }
-    var path = './static/common/js/';
+    var path = './js/';
     ////jQueryフレームワーク
     //document.write( importSrc( path + 'jquery-1.3.2.min.js' ) );
     //サイト全体で使用するJS
-    document.write( importSrc( path + '/loadHTML.js' ) );
-
-    //読み込むモジュール
-    importModule("trump")
+    document.write( importSrc( path + 'card.js' ) );
+    document.write( importSrc( path + 'trump.js' ) );
 })();
-
-function getCurrentDirectoryPath(){
-  alert(location.href.split("/"))
-}
